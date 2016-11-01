691a2f0dbc0d254fd4e24a2f334d50b20def7981
Michael_Shliselberg <mshliselberg@umass.edu>
2016-11-01 16:54:08 -0400

    adding server header/ struct implementations

---
+++ session-structs.cpp
@@ -0,0 +1,23 @@
+include "session-structs.h"
+
+typedef struct team{
+	char color;
+	player_t *playerArr;
+	int teamPoints;
+}team_t;
+
+typedef struct base{
+	double x;
+	double y;
+	char c;
+	int score
+}base_t;
+
+
+typedef struct player{
+	long int id;
+	int points;
+}
+
+
+
