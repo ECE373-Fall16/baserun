e6e7633f42dc095fc96bc7adf606473d93f2c403
Daniel Bergman <dbergman@umass.edu>
2016-11-01 21:16:42 -0400

    Created basic framework for UI-Client interaction

--- BaseRunClient.java
+++ BaseRunClient.java
@@ -1,6 +1,39 @@
 
 public class BaseRunClient{
+	int id;
+	boolean inGame;
+	Game curr;
 	public static void  main(String[] args){
-	
+		try{
+			Scanner uid = new Scanner(new FileReader("uid.db"));
+			id = uid.nextInt();
+		} catch(Exception e){
+			PrintWriter save = new PrintWriter("uid.db","UTF-8");
+			id = /*Contact server for new UID*/;
+			save.println(id);
+		}
+		//Connect user to server//
+		//After connected, as user changes pages allow for user inputs//
+		if(/*CREATE GAME*/){
+			//Server Call to create game//
+		} else if(/*JOIN GAME*/){
+			if(/*GameList*/){
+				//PrintGameList to select//
+				//Server call to join selected game//
+				if(/*Game is joined*/){
+					inGame == true;
+					curr = new Game(/*Info from server*/);
+				}
+			} else if (/*GameID*/){
+				//Server call to join game//
+				if(/*Game is joined*/){
+					inGame == true;
+					curr = new Game(/*Info from server*/);
+				}
+			}
+			//Refresh game until starts//
+		} else if(/*ON GAME SCREEN*/&& inGame){
+			//PLAY GAME BASED ON GAME RULES//
+		}
 	}
 }
