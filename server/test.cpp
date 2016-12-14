f6af26099a5b9c9e521613b0d90b8f2c6a05162d
Michael_Shliselberg <mshliselberg@umass.edu>
2016-12-13 23:14:53 -0500

    help tests for pat

---
+++ server/test.cpp
@@ -0,0 +1,39 @@
+//header w/o gaurd
+#include <cassert>
+#include <stdexcept>
+#include <iostream>
+#include <unistd.h>
+#include <string>
+#include <iomanip>
+
+//header gaurds
+#include "xmlrpc-c/base.hpp"
+#include "xmlrpc-c/registry.hpp"
+#include "xmlrpc-c/server_abyss.hpp"
+
+#ifndef sessionList_class_h
+#define sessionList_class_h
+#include "sessionList-class.h"
+#endif
+
+#ifndef session_class_h
+#define session_class_h
+#include "session-class.h"
+#endif
+
+#ifndef session_structs_h
+#define sessions_structs_h
+#include "session-structs.h"
+#endif
+
+#ifndef team_class_h
+#define team_class_h
+#include "team-class.h"
+#endif
+
+#ifndef linkedList_h
+#define linkedList_h
+#include "linkedList.h"
+#endif
+
+
